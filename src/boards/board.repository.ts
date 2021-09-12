e6376389d805edab41bf0e4f949a5290a20c7094
ohasis <dhgkdud4002@naver.com>
2021-09-12 18:45:58 +0900

    데이터 베이스 연동 CRUD

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,7 +1,25 @@
 import { EntityRepository, Repository } from "typeorm";
+import { BoardStatus } from "./board-status.enum";
 import { Board } from "./board.entity";
+import { CreateBoardDto } from "./dto/create-board.dto";
 
 @EntityRepository(Board) //Board를 컨트롤하는 repository임을 선언
 export class BoardRepository extends Repository<Board> {
 
+    async createBoard(createBoardDto: CreateBoardDto): Promise <Board> {
+        
+        //const title = createBoardDto.title;
+        const {title,description} = createBoardDto;
+
+        const board = this.create({
+            title,
+            description, // : 안해줘도 두개 이름 똑같으면 지워줘도 댐
+            status: BoardStatus.PUBLIC
+        })
+
+        await this.save(board);
+        return board;
+    }
+
+    //async deleteBoard
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -2,8 +2,55 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { BoardStatus } from './board-status.enum';
 import { v1 as uuid } from 'uuid'; //v1: version
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardRepository } from './board.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './board.entity';
 
 @Injectable()
 export class BoardsService {
+    constructor(
+        @InjectRepository(BoardRepository)
+        private boardRepository : BoardRepository
+    ){}
+
+    async getAllBoards(): Promise <Board[]>{
+        return this.boardRepository.find();
+    }
+
+    createBoard(createBoardDto: CreateBoardDto): Promise <Board> {
+        
+        return this.boardRepository.createBoard(createBoardDto);
+    }
+    
+    //Id로 게시물 찾기
+    //typeORM에서 제공하는 findOne() 사용
+    async getBoardById(id:number): Promise <Board> {
+
+        const found = await this.boardRepository.findOne(id);
+
+        if(!found){
+            throw new NotFoundException(`Can't find Board with id ${id}`);
+        }
+        return found;
+    }
+
+    //Id로 특정 게시물 삭제하기
+    async deleteBoard(id:number) : Promise<void> {
+
+        const result = await this.boardRepository.delete(id);
+
+        if(result.affected === 0 ){
+            throw new NotFoundException(`Can't find Board with id ${id}`);
+        }
+        
+    }
+
+    async updateBoardStatus(id:number, status:BoardStatus): Promise <Board> {
+        const board = await this.getBoardById(id);
+        board.status = status;
+        await this.boardRepository.save(board);
+
+        return board;
+    }
     
 }
